38bba150dcc7db5ab1cfd94a7c302386a8cb741d
theBackendEngineer <70214314+theBackendEngineer@users.noreply.github.com>
2022-12-19 19:49:08 -0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from my import get_shape,show_accuracy,set_get_X,set_get_Y,set_dataframe,remove_
 from my import model_LogisticRegression,model_KNeigh,model_SVM,model_kernelSVM,model_NaiveBayes,model_DecisionTree,model_RandomForest
 #Defining App
 app = Flask(__name__,static_url_path='/static')
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
